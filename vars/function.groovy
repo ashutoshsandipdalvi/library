95713e56e7bd619673d55fe986d67343d12b89d5
ashutoshsandipdalvi <127596452+ashutoshsandipdalvi@users.noreply.github.com>
2023-03-20 17:03:40 +0530

    Update function.groovy

--- vars/function.groovy
+++ vars/function.groovy
@@ -2,3 +2,11 @@ def desc_func(new_var)
 {
   git "https://github.com/ashutoshsandipdalvi/${new_var}"
 }
+def package_func()
+{
+  sh "mvn package"
+}
+def deploy_func(job,ip,sample_path)
+{
+  sh "scp /var/lib/jenkins/workspace/${job}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${sample_path}"
+}
